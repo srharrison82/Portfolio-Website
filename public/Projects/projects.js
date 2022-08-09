6be85b2eed9d3d2a8e64ed18d5eba6c1f61fb66e
srharrison82 <srharrison82@gmail.com>
2022-08-09 11:40:59 +0100

    Forgot to change JS files with updates class tags

--- public/Projects/projects.js
+++ public/Projects/projects.js
@@ -1,7 +1,7 @@
 //Nav
 
-const toggleButton = document.getElementsByClassName("toggle-button")[0];
-const links = document.getElementsByClassName("links")[0];
+const toggleButton = document.getElementsByClassName("nav__toggle-btn")[0];
+const links = document.getElementsByClassName("nav__links")[0];
 
 toggleButton.addEventListener("click", function () {
   links.classList.toggle("active");
